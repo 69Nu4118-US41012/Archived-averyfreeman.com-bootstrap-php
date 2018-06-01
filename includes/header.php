5ede06c721b3b0bde3a6a311f75cc32ad266838f
Avery Freeman <avery@webtool.space>
2018-06-01 01:24:45 -0700

    Added 'articles' dropdown to navbar for external links and one article I've written

--- includes/header.php
+++ includes/header.php
@@ -34,6 +34,21 @@
                 <li class="<?php if ($section == "contact") {echo "active"; } ?>"><a href="mailform.php">Contact</a></li>
                 </li>
                 </li>
-            </ul>         
-        </div>
-    </nav>
+            <!--</ul>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">-->
+                <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Articles<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="https://forums.freebsd.org/threads/how-to-use-freebsd-for-recording-and-streaming-hdhomerun-tv.66054" target="_blank">HDHomeRun RECORD TV server on FreeBSD</a></li>
+                    <!--<li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>-->
+                </ul>            
+            </li>
+        </ul>        
+     </div>
+</nav>
